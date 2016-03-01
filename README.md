7e4983ab022ccff06150bc7cbe47f66eb61e5118
Patrick Steadman <ptsteadman@gmail.com>
2016-02-29 21:32:05 -0500

    Add license, remove cruft

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # College Memoir
-This is the LaTeX source of a novel that will be released as a PDF.  This
-document is primarily a work of fiction, and is a work in progress.  Feel free
+This is the LaTeX source of a novel that will be released as a PDF.  Feel free
 to make pull requests, raise issues, or fork this repository.
 
--- scripts/scripts.py
+++
@@ -1,7 +0,0 @@
-import nltk
-sentence = """Amethyst Amelia Kelly, better known by her stage name Iggy Azalea, has announced plans to spend an entire week in a KFC eating fried chicken wings after being dumped by her
-manager, rapper and music mogul T.I., because she 'needs time to think'."""
-tokens = nltk.word_tokenize(sentence)
-print tokens
-tagged = nltk.pos_tag(tokens)
-print tagged
